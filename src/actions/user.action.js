45a025ff2558fe8e417252635efb5a354d7dd1d1
hoc2k1 <kth100501@gmail.com>
2024-12-20 05:30:25 +0700

    update code

--- src/actions/user.action.js
+++ src/actions/user.action.js
@@ -182,9 +182,6 @@ export const editAddress = (data) => async (dispatch, getState) => {
   toast.success("Chỉnh sửa địa chỉ thành công!")
   return true
 }
-export const resetIsLogin = () => ({
-  type: userTypes.RESET_IS_LOGIN
-})
 export const logout = () => (dispatch, getState) => {
   storeConfig.clear()
   dispatch(addNewCart())
@@ -257,19 +254,6 @@ export const setLoginFail = () => ({
   data: 'login fail'
 })
 
-export const forgotEmailSuccess = () => ({
-  type: userTypes.FORGOT_EMAIL_SUCCESS
-})
-export const forgotEmailFail = () => ({
-  type: userTypes.FORGOT_EMAIL_FAIL
-})
-export const resetForgotPassword = () => ({
-  type: userTypes.RESET_FORGOT_PASSWORD
-})
-export const setEmailForgotPassword = (email) => ({
-  type: userTypes.SET_EMAIL_FORGOTPASSWORD,
-  email
-})
 export const setUser = (data) => ({
   type: userTypes.SET_USER,
   data
@@ -281,38 +265,4 @@ export const setAddresses = (data) => ({
 export const setAddressesLoading = (data) => ({
   type: userTypes.SET_ADDRESSES_LOADING,
   data
-})
-export const submitForgotPassword = (email) => async (dispatch, getState) => {
-  let res
-  try {
-    res = await axios.get(`${url.URL_BE}user/request/forgotpassword/` + email)
-  }
-  catch (err) {
-    dispatch(forgotEmailFail())
-    return
-  }
-  dispatch(setEmailForgotPassword(res.data.email))
-  dispatch(forgotEmailSuccess())
-}
-export const submitEnterNewPassword = (newPassword) => async (dispatch, getState) => {
-  let res
-  try {
-    res = await axios.post(`${url.URL_BE}user/forgotpassword`, {
-      email: getState().userReducers.forgotPassword.email,
-      otp: getState().userReducers.forgotPassword.otp,
-      newPassword: newPassword
-    })
-  }
-  catch (err) {
-    dispatch(forgotPasswordFail())
-    return
-  }
-  dispatch(forgotPasswordSuccess())
-}
-
-export const forgotPasswordSuccess = () => ({
-  type: userTypes.FORGOT_PASSWORD_SUCCESS
-})
-export const forgotPasswordFail = () => ({
-  type: userTypes.FORGOT_PASSWORD_FAIL
 })
--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -25,11 +25,6 @@ const login = (state = initial, action) => {
         islogin: false
       }
     }
-    case userTypes.RESET_IS_LOGIN:
-      return {
-        ...state,
-        islogin: null
-      }
     case userTypes.SET_LOADING_LOGIN_REGISTER:
       return {
         ...state,
@@ -38,57 +33,6 @@ const login = (state = initial, action) => {
     default: {
       return state
     }
-  }
-}
-const forgotPassword = (state = {}, action) => {
-  switch (action.type) {
-    case userTypes.FORGOT_EMAIL_SUCCESS: {
-      return {
-        ...state,
-        isForgot: true
-      }
-    }
-    case userTypes.FORGOT_EMAIL_FAIL: {
-      return {
-        ...state,
-        isForgot: false
-      }
-    }
-    case userTypes.SET_EMAIL_FORGOTPASSWORD: {
-      return {
-        ...state,
-        email: action.email
-      }
-    }
-    case userTypes.VERIFY_OTP_SUCCESS: {
-      return {
-        ...state,
-        verify_otp: true,
-        otp: action.otp
-      }
-    }
-    case userTypes.VERIFY_OTP_FAIL: {
-      return {
-        ...state,
-        verify_otp: false
-      }
-    }
-    case userTypes.FORGOT_PASSWORD_SUCCESS: {
-      return {
-        ...state,
-        forgotpassword: true
-      }
-    }
-    case userTypes.FORGOT_PASSWORD_FAIL: {
-      return {
-        ...state,
-        forgotpassword: false
-      }
-    }
-    case userTypes.RESET_FORGOT_PASSWORD: {
-      return {}
-    }
-    default: return state
   }
 }
 const user = (state = {}, action) => {
